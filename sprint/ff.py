e00f491fba8c6a313f4e88ea0c64f69858f73621
Sameer Kashyap <41501069+MistakenPirate@users.noreply.github.com>
2025-05-09 16:32:05 +0530

    CHORE: follow

--- sprint/ff.py
+++ sprint/ff.py
@@ -36,3 +36,41 @@ for nt in non_terminals:
 
 for nt in non_terminals:
     print(f"FIRST({nt}) : {sorted(first[nt])}")
+
+def com_follow():
+    while True:
+        updated = False
+
+        for nt in non_terminals:
+            for prod in grammar[nt]:
+                symbols = prod.split()
+
+                trailer = follow[nt].copy()
+
+                for symbol in reversed(symbols):
+                    if symbol in non_terminals:
+
+                        before = len(follow[symbol])
+
+                        follow[symbol].update(trailer)
+
+                        after = len(follow[symbol])
+
+                        if before < after:
+                            updated = True
+
+                        if 'e' in first[symbol]:
+                            trailer.update(first[symbol] - {'e'})
+                        else:
+                            trailer = first[symbol].copy()
+
+                    elif symbol in terminals:
+                        trailer = {symbol}
+
+        if not updated:
+            break
+
+com_follow()
+
+for nt in non_terminals:
+    print(f"FOLLOW({nt}) : {sorted(follow[nt])}")
